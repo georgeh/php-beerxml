2530cb1a869f7541e6e6e2dec8dc50a67fa1876b
George Hotelling <george@hotelling.net>
2013-04-23 22:58:10 -0400

    Updated docs

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 php-beerxml
 ===========
 
-A PHP BeerXML Parser and Generator
+A PHP parser and generator for the [BeerXML](http://www.beerxml.com/) standard.
+
+Installing
+==========
+
+Add `"georgeh/php-beerxml"` to your composer.json and run `composer update`. Not using Composer? Get it at http://getcomposer.org/
+  
+Usage
+=====
+
+Parser
+------
+
+```php
+require_once('vendor/autoload.php');
+
+$parser = new \BeerXML\Parser();
+$parser->setXmlString(file_get_contents('http://www.beerxml.com/recipes.xml'));
+$result = $parser->parse();
+foreach ($result as $recipe) {
+    /** @var $recipe \BeerXML\Record\Recipe **/
+    echo "Found beer recipe " . $recipe->getName() . "\n";
+}
+```
+
+Generator
+---------
+
+Coming soon!
---
+++ src/BeerXML/Parser/Equipment.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace BeerXML\Parser;
+
+
+class Equipment extends Record {
+    protected $tagName = 'EQUIPMENT';
+
+    /**
+     * Tags that map to simple values and the corresponding setter method on the record class
+     * @var array
+     */
+    protected $simpleProperties = array(
+        'NAME'              => 'setName',
+        'VERSION'           => 'setVersion',
+        'BOIL_SIZE'         => 'setBoilSize',
+        'BATCH_SIZE'        => 'setBatchSize',
+        'TUN_VOLUME'        => 'setTunVolume',
+        'TUN_WEIGHT'        => 'setTunWeight',
+        'TUN_SPECIFIC_HEAT' => 'setTunSpecificHeat',
+        'TOP_UP_WATER'      => 'setTopUpWater',
+        'TRUB_CHILLER_LOSS' => 'setTrubChillerLoss',
+        'EVAP_RATE'         => 'setEvapRate',
+        'BOIL_TIME'         => 'setBoilTime',
+        'CALC_BOIL_VOLUME'  => 'setCalcBoilVolume',
+        'LAUTER_DEADSPACE'  => 'setLauterDeadspace',
+        'TOP_UP_KETTLE'     => 'setTopUpKettle',
+        'HOP_UTILIZATION'   => 'setHopUtilization',
+        'NOTES'             => 'setNotes',
+    );
+
+    protected function createRecord()
+    {
+        return new \BeerXML\Record\Equipment();
+    }
+}
